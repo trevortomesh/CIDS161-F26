1b759feaaa89b772fd376849da83dd56a5e90385
Trevor Tomesh <slugzzz@gmail.com>
2026-09-14 15:52:55 -0500

    First commit!

---
+++ main.py
@@ -0,0 +1,18 @@
+# Print out some text
+# print("Too much coffee!")
+# print("No such thing!")
+# print("Let's have more!")
+
+print("2 + 2 = ", 2+2)
+
+print("2 x 2 = ", 2*2)
+
+print("2 / 2 = ", 2/2)
+
+print("2 - 2 = ", 2-2)
+
+
+
+# print(2+2)
+
+

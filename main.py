b26f58d5cc48085f6433d5f4e9586c2d7cfe54ae
Trevor Tomesh <slugzzz@gmail.com>
2026-09-16 09:57:26 -0500

    sept 16 class

--- main.py
+++ main.py
@@ -3,16 +3,27 @@
 # print("No such thing!")
 # print("Let's have more!")
 
-print("2 + 2 = ", 2+2)
+#print("2 + 2 = ", 2+2)
 
-print("2 x 2 = ", 2*2)
+#print("2 x 2 = ", 2*2)
 
-print("2 / 2 = ", 2/2)
+#print("2 / 2 = ", 2/2)
 
-print("2 - 2 = ", 2-2)
+#print("2 - 2 = ", 2-2)
 
 
 
 # print(2+2)
 
+#-----------------------------------------------------
+# Compute the area of a circle
 
+# ask user for the radius
+#radius = 20 # radius is now 20
+radius = eval(input("Enter a value for radius: "))
+
+# compute area
+area = radius * radius * 3.14159
+
+# print result
+print("The area for the circle of radius",radius,"is",area)
